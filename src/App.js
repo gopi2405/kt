0693c87c3cd4dc609e67733bb4ca9e114af77aaf
gopi2405 <mgopireddy0@gmail.com>
2023-03-06 12:27:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-// import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import { useEffect } from "react";
+
 function App() {
-  const [name, setName] = useState('');
-  const [name2, setName2] = useState('');
+
+  useEffect(() => {
+
+    fetch('http://localhost:4000/post', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then(
+      response => response.json()
+    ).then(data => console.log(data))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <form action="../../post" method="post"
-          className="form">
-          <label>name:</label>
-          <input value={name} onChange={e => setName(e.target.value)} id='name' name='name' /><br />
-          <label>name2:</label>
-          <input value={name2} onChange={e => setName2(e.target.value)} id='name' name='name2' /><br />
-          <button type="submit" >click</button>
-        </form>
-      </header>
+    <div>
+      helloworld
     </div>
   );
 }
