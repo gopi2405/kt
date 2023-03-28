da5c5703b2e3469eb6342b3ce0e4cf3bc4b5160d
gopi2405 <mgopireddy0@gmail.com>
2023-03-28 17:18:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [num, setNum] = useState('');
+  const [num2, setNum2] = useState('');
+  const [sum, setSum] = useState(null);
+  const [data, setData] = useState([]);
+  const n1 = Number(num)
+  const n2 = Number(num2)
 
+
+  const addRow = () => {
+    const obj = { n1, n2 };
+    setData([...data, obj]);
+  };
   useEffect(() => {
 
     fetch('http://localhost:4000/post', {
@@ -11,12 +22,48 @@ function App() {
       }
     }).then(
       response => response.json()
-    ).then(data => console.log(data))
+    ).then(data => console.log(data)).catch(err => {
+      console.log(err);
+      return {} //(or [], or an empty return, or any return at all)
+    })
   }, []);
 
   return (
+
     <div>
-      helloworld
+      <h1> </h1><br />
+      <label>
+        enter number:
+        <input
+          value={num}
+          onChange={e => setNum(e.target.value)}
+          type="number"
+        />
+      </label><br /><br />
+      <label>
+        enter number:
+        <input
+          value={num2}
+          onChange={e => setNum2(e.target.value)}
+          type="number"
+        />
+      </label><br /><br />
+      <button onClick={() => setSum(n1 * n2)}>
+        *
+      </button>
+      <button onClick={() => setSum(n1 + n2)}>
+        +
+      </button>
+      <button onClick={addRow}>
+        arr
+      </button>
+      {/* <p>{data.map((emp, index) => (
+        <div key={index}>
+          <h3>{emp.n1}</h3>
+        </div>
+      ))}</p> */}
+      <p>{JSON.stringify(data)}</p>
+      <p>{sum}</p>
     </div>
   );
 }
